5de5c72e5df3eea91c8522f2929c874891aff14f
dhakidd44 <119883802+dhakidd44@users.noreply.github.com>
2023-10-24 15:21:24 -0300

    ajout des vue pour afficher nos donnees capturees

--- myProject/myApp/urls.py
+++ myProject/myApp/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.lire_et_enregistrer, name="lire_et_enregistrer")
+    path("", views.lire_et_enregistrer, name="lire_et_enregistrer"),
+    path("", views.afficher_graphique, name="afficher_graphique")
 ]
--- myProject/myApp/views.py
+++ myProject/myApp/views.py
@@ -1,11 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 from .models import DonneeCapteur
 import serial
 # Create your views here.
 def lire_et_enregistrer(request):
-    context ={}
     try:
-        port = serial.Serial('/dev/ttyACM0', 9600)  # Assurez-vous d'ajuster le port série selon votre configuration
+        port = serial.Serial('COM9', 9600)  # Assurez-vous d'ajuster le port série selon votre configuration
         donnees = port.readline()
         donnees = float(donnees.decode().strip())  # Convertir les données en float
         port.close()
@@ -14,6 +13,19 @@ def lire_et_enregistrer(request):
         donnee_capteur = DonneeCapteur(temperature=donnees)
         donnee_capteur.save()
 
+        context ={'donnees': donnees}
         return render(request, "myApp/index.html", context)
     except serial.SerialException:
-        return render("Erreur de communication avec l'Arduino")
+        return HttpResponse("Erreur de communication avec l'Arduino")
+    from .models import DonneeCapteur
+
+def afficher_graphique(request):
+    donnees = DonneeCapteur.objects.all()
+    labels = [str(d.timestamp) for d in donnees]
+    valeurs = [d.temperature for d in donnees]
+
+    context = {
+        'labels': labels,
+        'valeurs': valeurs,
+    }
+    return render(request, "myApp/graphique.html", context)

924a92eda03c15ee8f58b549aaf1895164ef0355
dhakidd44 <119883802+dhakidd44@users.noreply.github.com>
2023-10-24 14:50:16 -0300

    Création d' un modèle Django pour stocker les données du capteur dans la base de données.

--- myProject/myApp/models.py
+++ myProject/myApp/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here.
+class DonneeCapteur(models.Model):
+    temperature = models.FloatField()
+    timestamp = models.DateTimeField(auto_now_add=True)

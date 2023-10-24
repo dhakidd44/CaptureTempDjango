0e8425b894e2e941e354dd43d3f85c9bdf602cfc
dhakidd44 <119883802+dhakidd44@users.noreply.github.com>
2023-10-24 14:56:46 -0300

    Création d'une vue

--- myProject/myApp/views.py
+++ myProject/myApp/views.py
@@ -1,6 +1,19 @@
 from django.shortcuts import render
-
+from .models import DonneeCapteur
+import serial
 # Create your views here.
-def home(request):
+def lire_et_enregistrer(request):
     context ={}
-    return render(request, "myApp/index.html", context)
+    try:
+        port = serial.Serial('/dev/ttyACM0', 9600)  # Assurez-vous d'ajuster le port série selon votre configuration
+        donnees = port.readline()
+        donnees = float(donnees.decode().strip())  # Convertir les données en float
+        port.close()
+        
+        # Enregistrez les données dans la base de données
+        donnee_capteur = DonneeCapteur(temperature=donnees)
+        donnee_capteur.save()
+
+        return render(request, "myApp/index.html", context)
+    except serial.SerialException:
+        return render("Erreur de communication avec l'Arduino")
